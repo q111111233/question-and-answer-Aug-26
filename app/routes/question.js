06cc99d90c95c90f71776cbd46fb0a204bd4db2f
Bo Zhao <zhaobo419@gmail.com>
2016-08-19 16:24:33 -0700

    allow user to edit question

--- app/routes/question.js
+++ app/routes/question.js
@@ -13,6 +13,16 @@ export default Ember.Route.extend({
         return question.save();
       });
       this.transitionTo('question', params.question);
-    }
+    },
+
+    update(question, params){
+      Object.keys(params).forEach(function(key){
+        if(params[key]!==undefined){
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
   }
 });
