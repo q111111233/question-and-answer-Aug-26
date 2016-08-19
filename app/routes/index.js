90ba7e5e6036e00e46847c6d23429fc9beeebafe
Bo Zhao <zhaobo419@gmail.com>
2016-08-19 14:24:10 -0700

    add new question

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll("question");
+    return Ember.RSVP.hash({
+      question: this.store.findAll('question'),
+      answer: this.store.findAll('answer')
+    });
   },
+  actions:{
+    save(params){
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
