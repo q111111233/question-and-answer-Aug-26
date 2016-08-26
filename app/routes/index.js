e947c22bcadc2db81d7a926c2ca542ba88c97968
Bo Zhao <zhaoo419@gmail.com>
2016-08-26 15:53:27 -0700

    add favorite question list

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  favoriteQuestions: Ember.inject.service(),
   model(){
     return Ember.RSVP.hash({
       questions: this.store.findAll('question'),
@@ -12,6 +13,9 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
+    },
+    addToFavorite(question) {
+      this.get('favoriteQuestions').add(question);
     }
   }
 });
