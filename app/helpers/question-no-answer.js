cbce175c9428b7d79df61fb7bb66c31403ac812f
Bo Zhao <zhaoo419@gmail.com>
2016-08-26 14:34:50 -0700

    add phone icon on no-answer-question and earphone icon on answered question

---
+++ app/helpers/question-no-answer.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export function questionNoAnswer(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') == 0) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-phone-alt"></span>');
+  }
+  else{
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-earphone"></span>');
+  }
+}
+
+export default Ember.Helper.helper(questionNoAnswer);
---
+++ tests/unit/helpers/question-no-answer-test.js
@@ -0,0 +1,10 @@
+import { questionNoAnswer } from 'question-and-answer/helpers/question-no-answer';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question no answer');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionNoAnswer([42]);
+  assert.ok(result);
+});
